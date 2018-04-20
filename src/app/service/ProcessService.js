762c251b9839aeed337fb2ea0222bd06979edc94
liangxiaojun <fatalxiao@163.com>
2018-04-20 08:34:22 +0800

    update files

--- src/app/service/ProcessService.js
+++ src/app/service/ProcessService.js
@@ -1,7 +1,18 @@
+import fs from 'fs';
+import path from 'path';
 import Response from '../utils/Response.js';
 
 async function getAll() {
-    return Response.buildSuccess();
+
+    const json = fs.readFileSync(path.resolve(__dirname, '../../processes.json'));
+
+    try {
+        const data = JSON.parse(json);
+        return Response.buildSuccess(data);
+    } catch (e) {
+        return Response.buildSuccess([]);
+    }
+
 };
 
 async function start(processId) {
