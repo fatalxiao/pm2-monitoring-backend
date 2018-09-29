8284e2f6681ff3771f90c83b796998e2c9ccc780
liangxiaojun <fatalxiao@163.com>
2018-09-29 09:38:42 +0800

    update ProcessService

--- src/app/service/ProcessService.js
+++ src/app/service/ProcessService.js
@@ -1,6 +1,17 @@
 import Response from '../utils/Response.js';
 import {getProcessesConfig} from '../utils/ProcessUtil.js';
-import {getPMList} from '../utils/PMUtil.js';
+import {
+    getPMList,
+    start,
+    stopById,
+    stopAll,
+    restartById,
+    restartAll,
+    delById,
+    delAll,
+    reloadById,
+    reloadAll
+} from '../utils/PMUtil.js';
 
 async function getProcesses() {
 
@@ -10,27 +21,118 @@ async function getProcesses() {
         return Response.buildSuccess([]);
     }
 
-    // get pm2 list data
-    const processList = await getPMList();
+    try {
 
-    return Response.buildSuccess(data.filter(item => item).map(item => {
-        const index = processList.findIndex(p => p && p.name === item.name);
-        return index === -1 ? item : {
-            ...item,
-            pid: processList[index].pid,
-            pm_id: processList[index].pm_id,
-            status: processList[index].pm2_env.status,
-            monit: processList[index].monit
-        };
-    }));
+        // get pm2 list data
+        const processList = await getPMList();
+
+        return Response.buildSuccess(data.filter(item => item).map(item => {
+            const index = processList.findIndex(p => p && p.name === item.name);
+            return index === -1 ? item : {
+                ...item,
+                pid: processList[index].pid,
+                pm_id: processList[index].pm_id,
+                status: processList[index].pm2_env.status,
+                monit: processList[index].monit
+            };
+        }));
+
+    } catch (e) {
+        return Response.buildError(e);
+    }
 
 };
 
-async function start(processId) {
-    return Response.buildSuccess();
+async function start(options) {
+    try {
+        const proc = await start(options);
+        return Response.buildSuccess(proc);
+    } catch (e) {
+        return Response.buildError(e);
+    }
+};
+
+async function stopById(processId) {
+    try {
+        const proc = await stopById(processId);
+        return Response.buildSuccess(proc);
+    } catch (e) {
+        return Response.buildError(e);
+    }
+};
+
+async function stopAll() {
+    try {
+        const proc = await stopAll();
+        return Response.buildSuccess(proc);
+    } catch (e) {
+        return Response.buildError(e);
+    }
+};
+
+async function restartById(processId) {
+    try {
+        const proc = await restartById(processId);
+        return Response.buildSuccess(proc);
+    } catch (e) {
+        return Response.buildError(e);
+    }
+};
+
+async function restartAll() {
+    try {
+        const proc = await restartAll();
+        return Response.buildSuccess(proc);
+    } catch (e) {
+        return Response.buildError(e);
+    }
+};
+
+async function delById(processId) {
+    try {
+        const proc = await delById(processId);
+        return Response.buildSuccess(proc);
+    } catch (e) {
+        return Response.buildError(e);
+    }
+};
+
+async function delAll() {
+    try {
+        const proc = await delAll();
+        return Response.buildSuccess(proc);
+    } catch (e) {
+        return Response.buildError(e);
+    }
+};
+
+async function reloadById(processId) {
+    try {
+        const proc = await reloadById(processId);
+        return Response.buildSuccess(proc);
+    } catch (e) {
+        return Response.buildError(e);
+    }
+};
+
+async function reloadAll() {
+    try {
+        const proc = await reloadAll();
+        return Response.buildSuccess(proc);
+    } catch (e) {
+        return Response.buildError(e);
+    }
 };
 
 export default {
     getProcesses,
-    start
+    start,
+    stopById,
+    stopAll,
+    restartById,
+    restartAll,
+    delById,
+    delAll,
+    reloadById,
+    reloadAll
 };
