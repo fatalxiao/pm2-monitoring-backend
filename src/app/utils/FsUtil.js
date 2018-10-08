a70f39ffae712c0845b88c0216ea0213ea335b44
liangxiaojun <fatalxiao@163.com>
2018-10-08 20:03:43 +0800

    update FsUtil

--- src/app/utils/FsUtil.js
+++ src/app/utils/FsUtil.js
@@ -9,6 +9,35 @@ function isExistSync(p) {
     return true;
 }
 
+function rmRecursionSync(p) {
+
+    const paths = fs.readdirSync(p);
+
+    for (let path of paths) {
+
+        const rmPath = p + '/' + path,
+            stat = fs.statSync(rmPath);
+
+        if (stat.isDirectory()) {
+            rmRecursionSync(rmPath);
+            if (isExistSync(rmPath)) {
+                fs.rmdirSync(rmPath);
+            }
+        } else {
+            if (isExistSync(rmPath)) {
+                fs.unlinkSync(rmPath);
+            }
+        }
+
+    }
+
+    if (isExistSync(p)) {
+        fs.rmdirSync(p);
+    }
+
+};
+
 export default {
-    isExistSync
+    isExistSync,
+    rmRecursionSync
 };
