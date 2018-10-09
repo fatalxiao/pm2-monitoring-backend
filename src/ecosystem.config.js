e2d9c8ecabbd63dfc491537e0177334139c7e801
liangxiaojun <fatalxiao@163.com>
2018-10-09 11:18:16 +0800

    update ecosystem.config

--- src/ecosystem.config.js
+++ src/ecosystem.config.js
@@ -1,12 +1,9 @@
 module.exports = {
     apps: [{
-        name: 'alcedo-ui',
-        script: './pm2-apps/alcedo-ui/server.js',
+        name: 'pm2-monitoring-backend',
+        script: './app.js',
         env: {
             NODE_ENV: 'development'
-        },
-        env_production: {
-            NODE_ENV: 'production'
         }
     }]
 };
