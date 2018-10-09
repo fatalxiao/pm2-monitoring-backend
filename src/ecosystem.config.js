e415487ee18080e238f82359b1f6fc67199327b0
liangxiaojun <fatalxiao@163.com>
2018-10-09 11:28:00 +0800

    update ecosystem.config

--- src/ecosystem.config.js
+++ src/ecosystem.config.js
@@ -3,7 +3,7 @@ module.exports = {
         name: 'pm2-monitoring-backend',
         script: 'app.js',
         env: {
-            NODE_ENV: 'development'
+            NODE_ENV: 'production'
         }
     }]
 };
