62dfaff05484343d2121b6090a822713dfb2e22b
liangxiaojun <fatalxiao@163.com>
2018-10-10 09:26:41 +0800

    update gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,7 @@ const gulp = require('gulp'),
     babel = require('gulp-babel');
 
 gulp.task('copy', () =>
-    gulp.src(['./src/**', '!./src/pm2-apps/**', '!./src/ecosystem.config.js'])
+    gulp.src(['./src/**', '!./src/pm2-apps/**'])
         .pipe(babel({
             presets: [['@babel/env', {modules: 'commonjs'}]],
             plugins: ['@babel/plugin-transform-runtime']
