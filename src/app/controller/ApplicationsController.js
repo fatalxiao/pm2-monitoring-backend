7adbc26a0757c403901e60a0b79911da0c6dd2da
liangxiaojun <fatalxiao@163.com>
2018-10-10 20:17:41 +0800

    update ApplicationsController

--- src/app/controller/ApplicationsController.js
+++ src/app/controller/ApplicationsController.js
@@ -3,7 +3,7 @@ import {WsGetMapping} from '../utils/ApiDecorator';
 
 class ApplicationsController {
 
-    @WsGetMapping({route: '/pm/applications'})
+    @WsGetMapping({route: '/ws/pm/applications'})
     static async getApplications(ctx) {
         ctx.websocket.on('message', () => {
             ApplicationsService.getApplications().then(data => ctx.websocket.send(data));
